3c2d2fbabf41833738280a59fb1febbcd31d0493
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 00:18:21 +0100

    schema openapi property renamed

--- pkg/v3/schema.go
+++ pkg/v3/schema.go
@@ -1,7 +1,7 @@
 package v3
 
 type Schema struct {
-	Openapi string    `json:"openapi" yaml:"openapi"` // Required
+	OpenApi string    `json:"openapi" yaml:"openapi"` // Required
 	Info    *Info     `json:"info" yaml:"info"`       // Required
 	Servers []*Server `json:"servers,omitempty" yaml:"servers,omitempty"`
 	Paths   *Path     `json:"paths" yaml:"paths"` // Required
