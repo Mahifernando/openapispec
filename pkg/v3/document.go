f8bf03fa3a5e98b3d640806ada45b94da72bce68
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 21:54:24 +0100

    add extensions to document

--- pkg/v3/document.go
+++ pkg/v3/document.go
@@ -9,4 +9,5 @@ type Document struct {
 	Security     *SecurityRequirement   `json:"security,omitempty" yaml:"security,omitempty"`
 	Tags         []*Tag                 `json:"tags,omitempty" yaml:"tags,omitempty"`
 	ExternalDocs *ExternalDocumentation `json:"externalDocs,omitempty" yaml:"externalDocs,omitempty"`
+	Extensions   map[string]interface{} `json:"-" yaml:"-"` // Allows extensions to the OpenAPI Schema. The field name MUST begin with x-, for example, x-internal-id. The value can be null, a primitive, an array or an object. Can have any valid JSON format value.
 }
