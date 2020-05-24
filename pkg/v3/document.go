2ba4a3549456a414957c9a8dbed5d38c8d21f4d0
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 11:18:37 +0100

    refactor

--- pkg/v3/document.go
+++ pkg/v3/document.go
@@ -1,9 +1,10 @@
 package v3
 
 type Document struct {
-	OpenApi    string     `json:"openapi" yaml:"openapi"` // Required
-	Info       *Info      `json:"info" yaml:"info"`       // Required
-	Servers    []*Server  `json:"servers,omitempty" yaml:"servers,omitempty"`
-	Paths      *Path      `json:"paths" yaml:"paths"` // Required
-	Components *Component `json:"Component,omitempty" yaml:"Component,omitempty"`
+	OpenApi    string               `json:"openapi" yaml:"openapi"` // Required
+	Info       *Info                `json:"info" yaml:"info"`       // Required
+	Servers    []*Server            `json:"servers,omitempty" yaml:"servers,omitempty"`
+	Paths      *Path                `json:"paths" yaml:"paths"` // Required
+	Components *Component           `json:"components,omitempty" yaml:"components,omitempty"`
+	Security   *SecurityRequirement `json:"security,omitempty" yaml:"security,omitempty"`
 }
