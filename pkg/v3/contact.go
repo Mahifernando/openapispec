57e9879500894acb550582dd24763cbef269f31b
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 21:55:42 +0100

    add extensions to contact

--- pkg/v3/contact.go
+++ pkg/v3/contact.go
@@ -1,7 +1,8 @@
 package v3
 
 type Contact struct {
-	Name  string `json:"name,omitempty" yaml:"name,omitempty"`
-	Url   string `json:"url,omitempty" yaml:"url,omitempty"`
-	Email string `json:"email,omitempty" yaml:"email,omitempty"`
+	Name       string                 `json:"name,omitempty" yaml:"name,omitempty"`
+	Url        string                 `json:"url,omitempty" yaml:"url,omitempty"`
+	Email      string                 `json:"email,omitempty" yaml:"email,omitempty"`
+	Extensions map[string]interface{} `json:"-" yaml:"-"` // Allows extensions to the OpenAPI Schema. The field name MUST begin with x-, for example, x-internal-id. The value can be null, a primitive, an array or an object. Can have any valid JSON format value.
 }
