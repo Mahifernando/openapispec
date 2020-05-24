d520b680c89c97fd517d1556bce6c7df91d62f04
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 21:54:44 +0100

    add extensions to info

--- pkg/v3/info.go
+++ pkg/v3/info.go
@@ -1,10 +1,11 @@
 package v3
 
 type Info struct {
-	Title          string   `json:"title" yaml:"title"` // Required
-	Description    string   `json:"description,omitempty" yaml:"description,omitempty"`
-	TermsOfService string   `json:"termsOfService,omitempty" yaml:"termsOfService,omitempty"`
-	Contact        *Contact `json:"contact,omitempty" yaml:"contact,omitempty"`
-	License        *License `json:"license,omitempty" yaml:"license,omitempty"`
-	Version        string   `json:"version" yaml:"version"` // Required
+	Title          string                 `json:"title" yaml:"title"` // Required
+	Description    string                 `json:"description,omitempty" yaml:"description,omitempty"`
+	TermsOfService string                 `json:"termsOfService,omitempty" yaml:"termsOfService,omitempty"`
+	Contact        *Contact               `json:"contact,omitempty" yaml:"contact,omitempty"`
+	License        *License               `json:"license,omitempty" yaml:"license,omitempty"`
+	Version        string                 `json:"version" yaml:"version"` // Required
+	Extensions     map[string]interface{} `json:"-" yaml:"-"`             // Allows extensions to the OpenAPI Schema. The field name MUST begin with x-, for example, x-internal-id. The value can be null, a primitive, an array or an object. Can have any valid JSON format value.
 }
