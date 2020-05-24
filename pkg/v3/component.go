b763549b36a97f9a330196437b36699e1197ec24
Mahi Fernando <mahifernando@gmail.com>
2020-05-24 10:57:14 +0100

    add components place holder struct

---
+++ pkg/v3/component.go
@@ -0,0 +1,5 @@
+package v3
+
+type Component struct {
+	Title string `json:"title" yaml:"title"` // todo
+}
--- pkg/v3/schema.go
+++ pkg/v3/schema.go
@@ -1,8 +1,9 @@
 package v3
 
 type Schema struct {
-	OpenApi string    `json:"openapi" yaml:"openapi"` // Required
-	Info    *Info     `json:"info" yaml:"info"`       // Required
-	Servers []*Server `json:"servers,omitempty" yaml:"servers,omitempty"`
-	Paths   *Path     `json:"paths" yaml:"paths"` // Required
+	OpenApi    string     `json:"openapi" yaml:"openapi"` // Required
+	Info       *Info      `json:"info" yaml:"info"`       // Required
+	Servers    []*Server  `json:"servers,omitempty" yaml:"servers,omitempty"`
+	Paths      *Path      `json:"paths" yaml:"paths"` // Required
+	Components *Component `json:"Component,omitempty" yaml:"Component,omitempty"`
 }
