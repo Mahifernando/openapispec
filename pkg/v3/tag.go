668ad5bf8f23435491287ff5da3fa4bec10b8ac6
Mahi Fernando <mahifernando@gmail.com>
2020-05-25 16:26:06 +0100

    add tag

--- pkg/v3/tag.go
+++ pkg/v3/tag.go
@@ -1,5 +1,8 @@
 package v3
 
 type Tag struct {
-	Title string `json:"title" yaml:"title"` // Required TODO
+	Name         string                 `json:"name" yaml:"name"` // Required
+	Description  string                 `json:"description,omitempty" yaml:"description,omitempty"`
+	ExternalDocs *ExternalDocumentation `json:"externalDocs,omitempty" yaml:"externalDocs,omitempty"`
+	Extensions   *Extension             `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
 }
