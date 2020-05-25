98103543dbbdb0d8df2ab0d5163d9dec4c4de022
Mahi Fernando <mahifernando@gmail.com>
2020-05-25 16:18:23 +0100

    add external documentation

--- pkg/v3/external_documentation.go
+++ pkg/v3/external_documentation.go
@@ -1,5 +1,7 @@
 package v3
 
 type ExternalDocumentation struct {
-	Title string `json:"title" yaml:"title"` // Required TODO
+	Url         string     `json:"url" yaml:"url"` // Required
+	Description string     `json:"description,omitempty" yaml:"description,omitempty"`
+	Extensions  *Extension `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
 }
