60dfaa9bd6832731627d0616df3425c2509b064d
Mahi Fernando <mahifernando@gmail.com>
2020-06-25 22:11:43 +0100

    add example

---
+++ pkg/v3/example.go
@@ -0,0 +1,9 @@
+package v3
+
+type Example struct {
+	Summary       string      `json:"summary,omitempty" yaml:"summary,omitempty"`
+	Description   string      `json:"description,omitempty" yaml:"description,omitempty"`
+	Value         interface{} `json:"value,omitempty" yaml:"value,omitempty"`
+	ExternalValue string      `json:"externalValue,omitempty" yaml:"externalValue,omitempty"`
+	Extensions    *Extension  `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
+}
