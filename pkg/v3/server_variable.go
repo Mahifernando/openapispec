b63495fcd407a61789b175a9502c7709246a7103
Mahi Fernando <mahifernando@gmail.com>
2020-06-25 22:12:44 +0100

    implement server variable

--- pkg/v3/server_variable.go
+++ pkg/v3/server_variable.go
@@ -1,7 +1,8 @@
 package v3
 
 type ServerVariable struct {
-	Enum        []string `json:"enum,omitempty" yaml:"enum,omitempty"`
-	Default     string   `json:"default" yaml:"default"` // Required
-	Description string   `json:"description,omitempty" yaml:"description,omitempty"`
+	Enum        []string   `json:"enum,omitempty" yaml:"enum,omitempty"`
+	Default     string     `json:"default" yaml:"default"` // Required
+	Description string     `json:"description,omitempty" yaml:"description,omitempty"`
+	Extensions  *Extension `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
 }
