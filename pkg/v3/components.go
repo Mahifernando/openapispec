b010a0e5298b3c86d5c6eef12c6f51aee21f801a
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 18:40:04 +0100

    renamed to components

--- pkg/v3/components.go
+++ pkg/v3/components.go
@@ -1,6 +1,6 @@
 package v3
 
-type Component struct {
+type Components struct {
 	Schemas    map[string]*Schema   `json:"schemas,omitempty" yaml:"schemas,omitempty"`
 	Responses  map[string]*Response `json:"responses,omitempty" yaml:"responses,omitempty"`
 	Extensions *Extension           `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
