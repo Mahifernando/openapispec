d1a43f79825571d299e59ee67314c91d9c602ca6
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 18:41:08 +0100

    implement path

--- pkg/v3/path.go
+++ pkg/v3/path.go
@@ -1,5 +1,6 @@
 package v3
 
 type Path struct {
-	todo string
+	PathItems  map[string]*PathItem `json:"-" yaml:"-"` // A relative path to an individual endpoint. The field name MUST begin with a forward slash (/) , for example, /pets/{petId} /pets/mine.
+	Extensions *Extension           `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
 }
