cfc93295b12c947916a13f9c1aa9ad84dd4dadbc
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 20:24:01 +0100

    add oauth flows

---
+++ pkg/v3/oauth_flows.go
@@ -0,0 +1,9 @@
+package v3
+
+type OAuthFlows struct {
+	Implicit          *OAuthFlow `json:"implicit,omitempty" yaml:"implicit,omitempty"`
+	Password          *OAuthFlow `json:"password,omitempty" yaml:"password,omitempty"`
+	ClientCredentials *OAuthFlow `json:"clientCredentials,omitempty" yaml:"clientCredentials,omitempty"`
+	AuthorizationCode *OAuthFlow `json:"authorizationCode,omitempty" yaml:"authorizationCode,omitempty"`
+	Extensions        *Extension `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
+}
