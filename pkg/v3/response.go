9f36502e2820ec2bdb7c1eaed47ec6f7b0d48ad4
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 18:41:59 +0100

    refactor response to link with reference object

--- pkg/v3/response.go
+++ pkg/v3/response.go
@@ -1,6 +1,13 @@
 package v3
 
+// An object to hold reusable 'Response Object', which is provided as Node or Reference ($ref) to a 'Response Object' definition
 type Response struct {
+	Reference string `json:"$ref" yaml:"$ref"` // Required if reference ($ref) to a definition
+	Node      ResponseNode
+}
+
+// Represents 'Response Object' as defined by the http://spec.openapis.org/oas/v3.0.3#response-object
+type ResponseNode struct {
 	Description string             `json:"description" yaml:"description"` // Required
 	Headers     map[string]*Header `json:"headers,omitempty" yaml:"headers,omitempty"`
 	Content     *MediaType         `json:"content,omitempty" yaml:"content,omitempty"`
