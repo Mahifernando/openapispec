57ce065dd388ecb88f3bf53f205c671f34af77e3
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 18:41:18 +0100

    add path item

---
+++ pkg/v3/path_item.go
@@ -0,0 +1,18 @@
+package v3
+
+type PathItem struct {
+	Ref         string       `json:"$ref,omitempty" yaml:"$ref,omitempty"`
+	Summary     string       `json:"summary,omitempty" yaml:"summary,omitempty"`
+	Description string       `json:"description,omitempty" yaml:"description,omitempty"`
+	Servers     []*Server    `json:"servers,omitempty" yaml:"servers,omitempty"`
+	Parameters  []*Parameter `json:"parameters,omitempty" yaml:"parameters,omitempty"`
+	Get         *Operation   `json:"get,omitempty" yaml:"get,omitempty"`
+	Put         *Operation   `json:"put,omitempty" yaml:"put,omitempty"`
+	Post        *Operation   `json:"post,omitempty" yaml:"post,omitempty"`
+	Delete      *Operation   `json:"delete,omitempty" yaml:"delete,omitempty"`
+	Options     *Operation   `json:"options,omitempty" yaml:"options,omitempty"`
+	Head        *Operation   `json:"head,omitempty" yaml:"head,omitempty"`
+	Patch       *Operation   `json:"patch,omitempty" yaml:"patch,omitempty"`
+	Trace       *Operation   `json:"trace,omitempty" yaml:"trace,omitempty"`
+	Extensions  *Extension   `json:"-" yaml:"-"` // The field name MUST begin with x-, for example, x-internal-id.
+}
