c39b1b0d542b4e547503039ca43df96f22226b0d
Mahi Fernando <mahifernando@gmail.com>
2020-06-27 18:40:26 +0100

    renamed reference to components

--- pkg/v3/document.go
+++ pkg/v3/document.go
@@ -6,7 +6,7 @@ type Document struct {
 	Info         *Info                  `json:"info" yaml:"info"`       // Required
 	Servers      []*Server              `json:"servers,omitempty" yaml:"servers,omitempty"`
 	Paths        *Path                  `json:"paths" yaml:"paths"` // Required
-	Components   *Component             `json:"components,omitempty" yaml:"components,omitempty"`
+	Components   *Components            `json:"components,omitempty" yaml:"components,omitempty"`
 	Security     []*SecurityRequirement `json:"security,omitempty" yaml:"security,omitempty"`
 	Tags         []*Tag                 `json:"tags,omitempty" yaml:"tags,omitempty"`
 	ExternalDocs *ExternalDocumentation `json:"externalDocs,omitempty" yaml:"externalDocs,omitempty"`
